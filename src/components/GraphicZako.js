66670ad8b982c1877ec423eb54ef32d272c8d1d9
SnowMac <maksym73@gmail.com>
2023-05-04 15:40:38 +0200

    styling mobile version

--- src/components/GraphicZako.js
+++ src/components/GraphicZako.js
@@ -6,10 +6,11 @@ const GraphicZako = () => {
     return (
         <div className="graphic-zako-container">
             <img src={graphicZakoImage} alt="Graphic Krk" />
+            <div className='graphic-content'>
             <a href="https://apartamenty-krakow.crusoe.info" target="_blank" rel="noopener noreferrer" className="goto-btn">Go to</a>
             <h2 className="graphic-text">Zakopane</h2>
             <button className="reservation-btn">Reservation</button>
-
+            </div>
         </div>
     );
 }
